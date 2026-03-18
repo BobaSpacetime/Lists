9246f5d5ef1fe86b4a7ffe66034fea7be68e5385
BobaSpacetime <163637926+BobaSpacetime@users.noreply.github.com>
2026-03-18 20:02:45 +0100

    Add files via upload

---
+++ list.py
@@ -0,0 +1,9 @@
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(matrix)
+print(len(matrix))
+print(len(matrix[0]))
+print(matrix[1][0])
+for i in range(3):
+    for j in range(3):
+        print(matrix[i][j], end = " ")
+    print("\n")
